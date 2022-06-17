5c3f0e96cbc58bf1fedeba5526a960ec4d505cfb
Joel Hughes <joel.hughes@generalassemb.ly>
2022-06-17 09:42:22 -0600

    finished codealong

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = require("liquid-express-views")(express())
 const fruits = require('./models/fruits.js')
+const rowdy = require('rowdy-logger')
+const routesReport = rowdy.begin(app)
+
+const methodOverride = require('method-override')
 
 app.use((req, res, next) => {
     console.log('I run for all routes');
@@ -14,6 +18,9 @@ app.use(express.urlencoded({
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
 
 app.use(express.json()) // This prepares our api to receive json data from the body of all incoming requests.
+
+// use methodOverride. We'll be adding q query parameter to our delete form named _method
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     // res.render("show")
@@ -49,6 +56,34 @@ app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     })
 })
 
+app.delete('/fruits/:indexOfFruitsArray', (req, res) => {
+    fruits.splice(req.params.indexOfFruitsArray, 1) //remove the item from the array
+    res.redirect('/fruits') // redirect back to index route
+
+})
+
+app.get('/fruits/:indexOfFruitsArray/edit', (req, res) => {
+    res.render(
+        'edit', 
+        {
+            fruit: fruits[req.params.indexOfFruitsArray], 
+            index: req.params.indexOfFruitsArray
+        }
+    )
+})
+
+app.put('/fruits/:indexOfFruitsArray', (req, res) => {
+    // console.log(req.body)
+    if (req.body.readyToEat === "on"){
+        req.body.readyToEat = true
+    } else {
+        req.body.readyToEat = false
+    }
+    fruits[req.params.indexOfFruitsArray].color = req.body.color
+    res.redirect('/fruits')
+})
+
 app.listen(3000, () => {
     console.log("listening on port 3000!")
+    routesReport.print()
 })
